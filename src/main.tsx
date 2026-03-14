36c099d1a82294d93dda33b078436bcec5bb1247
Rodion Groshev <rodion.groshev@gmail.com>
2026-03-14 20:51:03 +0100

    comment google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,22 +5,22 @@ import App from "./App.tsx";
 import "./index.css";
 
 // Google Analytics 4
-const gaId = import.meta.env.VITE_GA_ID || "G-FJ7CQQN5BQ";
-if (gaId) {
-  const s = document.createElement("script");
-  s.async = true;
-  s.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
-  document.head.appendChild(s);
-  (window as unknown as { dataLayer: unknown[] }).dataLayer =
-    (window as unknown as { dataLayer: unknown[] }).dataLayer || [];
-  const gtag = (...args: unknown[]) =>
-    (
-      (window as unknown as { dataLayer: unknown[] }).dataLayer as unknown[]
-    ).push(args);
-  (window as unknown as { gtag: typeof gtag }).gtag = gtag;
-  gtag("js", new Date());
-  gtag("config", gaId);
-}
+// const gaId = import.meta.env.VITE_GA_ID || "G-FJ7CQQN5BQ";
+// if (gaId) {
+//   const s = document.createElement("script");
+//   s.async = true;
+//   s.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+//   document.head.appendChild(s);
+//   (window as unknown as { dataLayer: unknown[] }).dataLayer =
+//     (window as unknown as { dataLayer: unknown[] }).dataLayer || [];
+//   const gtag = (...args: unknown[]) =>
+//     (
+//       (window as unknown as { dataLayer: unknown[] }).dataLayer as unknown[]
+//     ).push(args);
+//   (window as unknown as { gtag: typeof gtag }).gtag = gtag;
+//   gtag("js", new Date());
+//   gtag("config", gaId);
+// }
 
 createRoot(document.getElementById("root")!).render(
   <LanguageProvider>
